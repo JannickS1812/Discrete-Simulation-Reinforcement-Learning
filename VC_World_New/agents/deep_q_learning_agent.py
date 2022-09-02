677b9c3f24c249043d4f1c1fc40628ba92b479c3
jastr <jannick.stranghoener@fh-bielefeld.de>
2022-09-02 16:38:23 +0200

    fixed epsilon greedy strat

--- VC_World_New/agents/deep_q_learning_agent.py
+++ VC_World_New/agents/deep_q_learning_agent.py
@@ -221,7 +221,9 @@ class DeepQLearningAgentPlantSim(DeepQLearningAgent):
                         return self.q_table, self.N_sa
 
                     if np.random.random() < random_action:
-                        a = np.random.choice(self.actions)
+                        p = np.array(self.problem.filter_valid_actions(s_new))
+                        p /= p.sum()
+                        a = np.random.choice(self.actions, p=p)
                     else:
                         q_values = self.q_table[s_new] * np.array(self.problem.filter_valid_actions(s_new))
                         a = self.actions[np.argmax(q_values)]
