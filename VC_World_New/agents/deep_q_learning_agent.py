7316d166f33807adf1598469575a96676f62fffa
jastr <jannick.stranghoener@fh-bielefeld.de>
2022-09-02 15:41:27 +0200

    added filter to DQN

--- VC_World_New/agents/deep_q_learning_agent.py
+++ VC_World_New/agents/deep_q_learning_agent.py
@@ -218,7 +218,7 @@ class DeepQLearningAgentPlantSim(DeepQLearningAgent):
                     if is_goal_state:
                         return self.q_table, self.N_sa
 
-                    a = self.choose_GLIE_action(self.q_table[s_new], self.N_sa[s_new])
+                    a = self.choose_GLIE_action(self.q_table[s_new], self.N_sa[s_new], filter=self.problem.filter_valid_actions(s_new))
 
                     # act
                     self.problem.act(a)
@@ -226,4 +226,32 @@ class DeepQLearningAgentPlantSim(DeepQLearningAgent):
 
                     steps += 1
                     self.problem.unpause_simulation()
+
+    def choose_GLIE_action(self, q_values, N_s, filter=None):
+        exploration_values = np.ones_like(q_values) * self.R_Max
+        # which state/action pairs have been visited sufficiently
+        no_sufficient_exploration = N_s < self.max_N_exploration
+        # turn cost to a positive number
+        q_values_pos = self.R_Max / 2 + q_values
+        # select the relevant values (q or max value)
+        max_values = np.maximum(exploration_values * no_sufficient_exploration, q_values_pos)
+        # assure that we do not dived by zero
+        if max_values.sum() == 0:
+            probabilities = np.ones_like(max_values) / max_values.size
+        else:
+            probabilities = max_values / max_values.sum()
+
+        if filter is not None:
+            probabilities *= np.array(filter)
+
+        # select action according to the (q) values
+        if np.random.random() < (self.max_N_exploration + 0.00001) / (np.max(N_s) + 0.00001):
+            action = np.random.choice(self.actions, p=probabilities)
+        else:
+            action_indexes = np.argwhere(probabilities == np.amax(probabilities))
+            action_indexes.shape = (action_indexes.shape[0])
+            action_index = np.random.choice(action_indexes)
+            action = self.actions[action_index]
+        return action
+
 
